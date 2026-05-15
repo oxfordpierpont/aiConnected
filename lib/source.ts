fd45a9d586673591c88df85070a7c920fda11422
Oxford Pierpont <admin@oxpi.co>
2026-05-15 11:30:37 -0400

    Revert "Fix docs deployment build"
    This reverts commit a21940007a4196a589ac7489138e9be61e88ad52.

--- lib/source.ts
+++ lib/source.ts
@@ -1,4 +1,4 @@
-import { docs } from '../.source/server';
+import { docs } from 'collections/server';
 import { type InferPageType, loader } from 'fumadocs-core/source';
 import { lucideIconsPlugin } from 'fumadocs-core/source/lucide-icons';
 import { docsContentRoute, docsImageRoute, docsRoute } from './shared';
